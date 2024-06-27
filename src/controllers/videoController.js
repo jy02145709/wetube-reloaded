75be4c5ec1dae3b2b2fb2f26e2e84555c6247f3a
Glenn <112257932+jy02145709@users.noreply.github.com>
2024-06-28 01:09:21 +0900

    SchemaStaticAndDeleteVideo
    Model.static("name",function(name){
    function}
    
    const {id}=req.params
    async(req,res)=>{
    await model.findByIdAndDelete(id)
    }
    
    do not use remove

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -36,9 +36,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(",")
-      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   });
 
   return res.redirect(`/videos/${id}`);
@@ -52,8 +50,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags,
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      hashtags: Video.formatHashtags(hashtags),
     });
 
     return res.redirect("/");
@@ -63,4 +60,9 @@ export const postUpload = async (req, res) => {
       errorMessage: error._message,
     });
   }
+};
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
 };
