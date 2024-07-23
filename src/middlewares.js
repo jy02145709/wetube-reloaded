20c2ea19bb8d313a9ee4e53dc0314e7401e4454a
Glenn <112257932+jy02145709@users.noreply.github.com>
2024-07-24 03:47:25 +0900

    editProfile,ChangePassword
    edit profile protect url middleware and change password
    
    remember  update database and update the session

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,7 +1,21 @@
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
-  res.locals.loggedInUser = req.session.user;
-  console.log(res.locals);
+  res.locals.loggedInUser = req.session.user || {};
   res.locals.siteName = "Wetube";
   next();
+};
+
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
 };
